3fc5ab88e592d5cf76f67a08c8b86bbc4ec48030
mastertalent707-stack <mastertalent707@gmail.com>
2026-06-30 18:57:49 -0700

    chore(web): cleanup hook files and dead code (#1901)
    * refactor: extract refs to separate dir
    
    * refactor: extra hotkeys
    
    * refactor: move parsers
    
    * chore: delete unused usePointerState
    
    * chore: delete usePointerPosition
    
    * chore: delete useBufferedVisibility
    
    * chore: delete useCursorCoordinates
    
    * chore: move useMetaContext
    
    * refactor: cleanup ref context
    
    * refactor: move pointer files
    
    * chore: delete unused task migrations
    
    * test: fix parser test

--- packages/web/src/common/hooks/useCompassRefs.ts
+++
@@ -1,6 +0,0 @@
-import { CompassRefsContext } from "@web/common/context/compass-refs";
-import { useMetaContext } from "@web/common/hooks/useMetaContext";
-
-export function useCompassRefs() {
-  return useMetaContext(CompassRefsContext, "useCompassRefs");
-}
